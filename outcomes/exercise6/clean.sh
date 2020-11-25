bb9eb7385ee3aadecc9c2cdb5ebd62e886a6f0db
Nitin Bodke <nitin.bodke@broadcom.com>
2020-11-25 21:48:30 +0530

    1st Submission

--- outcomes/exercise6/clean.sh
+++ outcomes/exercise6/clean.sh
@@ -9,6 +9,9 @@ aws guardduty delete-detector --detector-id 2ebad68d89d1c8bcd1e516ce9e59107e
 aws s3 rm s3://cand-c3-free-recipes-745926752740 --recursive
 aws s3 rm s3://cand-c3-secret-recipes-745926752740 --recursive
 aws s3 rm s3://cand-c3-vpc-flow-logs-745926752740 --recursive
+aws s3 rm s3://cand-c3-primary-vpc-flow-logs-745926752740 --recursive
+aws s3 rm s3://cand-c3-default-vpc-flow-logs-745926752740 --recursive
+aws s3 rm s3://c3-s3-loggingbucket-ldqmy95xe91d --recursive
 
 # delete stacks
 aws cloudformation delete-stack --stack-name c3-app
